b1d82d70a0c8e752a88761632b4722ae14c753e5
Ondrey <andrzejwrzesinski.2004@gmail.com>
2026-03-04 10:32:17 +0100

    RomanNumerals class with not implemented convert function. First test for convert function with Failed result

---
+++ RomanNumerals/RomanNumerals/RomanNumerals.cs
@@ -0,0 +1,10 @@
+﻿namespace RomanNumerals
+{
+    public static class RomanNumerals
+    {
+        public static String convert(int num)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ RomanNumerals/RomanNumeralsTests/RomanNumeralsTests.cs
@@ -0,0 +1,13 @@
+using RomanNumerals;
+
+namespace RomanNumeralsTests
+{
+    public class RomanNumeralsTests
+    {
+        [Fact]
+        public void Test_Convert_1_I()
+        {
+            Assert.Equal("I", RomanNumerals.RomanNumerals.convert(1));
+        }
+    }
+}
