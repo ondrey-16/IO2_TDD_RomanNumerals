450d2b7fd599937c231ebf875eba6690a3dd5ac0
Ondrey <andrzejwrzesinski.2004@gmail.com>
2026-03-04 11:15:38 +0100

    Refactored tests - compressed to 1 test but with 8 different values

--- RomanNumerals/RomanNumeralsTests/RomanNumeralsTests.cs
+++ RomanNumerals/RomanNumeralsTests/RomanNumeralsTests.cs
@@ -4,36 +4,16 @@ namespace RomanNumeralsTests
 {
     public class RomanNumeralsTests
     {
-        [Fact]
-        public void Test_Convert_1_I()
-        {
-            Assert.Equal("I", RomanNumerals.RomanNumerals.convert(1));
-        }
-        [Fact]
-        public void Test_Convert_2_II()
-        {
-            Assert.Equal("II", RomanNumerals.RomanNumerals.convert(2));
-        }
-        [Fact]
-        public void Test_Convert_3_III()
-        {
-            Assert.Equal("III", RomanNumerals.RomanNumerals.convert(3));
-        }
-        [Fact]
-        public void Test_Convert_4_IV()
-        {
-            Assert.Equal("IV", RomanNumerals.RomanNumerals.convert(4));
-        }
-        [Fact]
-        public void Test_Convert_5_V()
-        {
-            Assert.Equal("V", RomanNumerals.RomanNumerals.convert(5));
-        }
         [Theory]
+        [InlineData("I", 1)]
+        [InlineData("II", 2)]
+        [InlineData("III", 3)]
+        [InlineData("IV", 4)]
+        [InlineData("V", 5)]
         [InlineData("VI", 6)]
         [InlineData("VII", 7)]
         [InlineData("VIII", 8)]
-        public void Test_Convert_6_To_8(String exp, int num)
+        public void Test_Convert_1_To_8(String exp, int num)
         {
             Assert.Equal(exp, RomanNumerals.RomanNumerals.convert(num));
         }
