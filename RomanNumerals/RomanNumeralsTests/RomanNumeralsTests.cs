8c07d5ee68b2455d796aad6fba14c783182ef24e
Ondrey <andrzejwrzesinski.2004@gmail.com>
2026-03-04 11:21:27 +0100

    Added more tests for the simplest numbers

--- RomanNumerals/RomanNumeralsTests/RomanNumeralsTests.cs
+++ RomanNumerals/RomanNumeralsTests/RomanNumeralsTests.cs
@@ -13,7 +13,22 @@ namespace RomanNumeralsTests
         [InlineData("VI", 6)]
         [InlineData("VII", 7)]
         [InlineData("VIII", 8)]
-        public void Test_Convert_1_To_8(String exp, int num)
+        [InlineData("IX", 9)]
+        [InlineData("X", 10)]
+        [InlineData("XXX", 30)]
+        [InlineData("XL", 40)]
+        [InlineData("L", 50)]
+        [InlineData("LX", 60)]
+        [InlineData("CD", 900)]
+        [InlineData("C", 100)]
+        [InlineData("CCC", 300)]
+        [InlineData("CD", 400)]
+        [InlineData("D", 500)]
+        [InlineData("DC", 600)]
+        [InlineData("CM", 900)]
+        [InlineData("M", 1000)]
+        [InlineData("MM", 2000)]
+        public void Test_Convert_Simple_Numbers(String exp, int num)
         {
             Assert.Equal(exp, RomanNumerals.RomanNumerals.convert(num));
         }
